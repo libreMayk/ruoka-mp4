cd308d1b7c75c2fc1262ace62a8c6294be285c2e
zeriaxdev <hedgegorg@gmail.com>
2022-03-13 13:19:16 +0200

    full server support

--- server.tsx
+++ server.tsx
@@ -1,10 +1,3 @@
-/**
- * This is an example of a server that returns dynamic video.
- * Run `npm run server` to try it out!
- * If you don't want to render videos on a server, you can safely
- * delete this file.
- */
-
 import {bundle} from '@remotion/bundler';
 import {
 	getCompositions,
@@ -16,36 +9,103 @@ import fs from 'fs';
 import path from 'path';
 import axios from 'axios';
 import moment from 'moment';
+import {JSDOM} from 'jsdom';
+import {CronJob} from 'cron';
 
 const clc = require('cli-color');
 
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8080;
 const compositionId = 'Food';
-
-const cache = new Map<string, string>();
 
 export const info = clc.bold.blue('[INFO] ');
 export const success = clc.bold.green('[SUCCESS] ');
 export const warning = clc.bold.yellow('[WARNING] ');
 export const error = clc.bold.red('[ERROR] ');
 
-const createVideo = async (inputProps?: object | undefined) => {
-	const bundled = await bundle(path.join(__dirname, './src/index.tsx'));
-	const comps = await getCompositions(bundled, {inputProps});
-	const video = comps.find((c) => c.id === compositionId);
-	if (!video) {
-		throw new Error(`No video called ${compositionId}`);
-	}
+const url = 'https://www.mayk.fi/tietoa-meista/ruokailu/';
 
+interface IFood {
+	date: string[];
+	normal: string[];
+	vege: string[];
+}
+
+const fetchData = () =>
+	new Promise<IFood | undefined>((resolve) => {
+		const start = Date.now();
+		console.info(info + `Fetching data...`);
+
+		axios(url)
+			.then((response) => {
+				const dom = new JSDOM(response.data);
+				const ruokaMenu = Array.from(
+					dom.window.document.querySelectorAll('.ruoka-template-header')
+				) as HTMLElement[];
+
+				const food: IFood = {
+					date: [],
+					normal: [],
+					vege: [],
+				};
+
+				ruokaMenu.map((element) => {
+					const ruokaPvm =
+						element.querySelector('.ruoka-header-pvm')?.textContent;
+					const ruoka = element.querySelector(
+						'.ruoka-header-ruoka'
+					)?.textContent;
+					const kasvisruoka = element.querySelector(
+						'.ruoka-header-kasvisruoka'
+					)?.textContent;
+
+					if (ruokaPvm && ruoka && kasvisruoka) {
+						food.date.push(ruokaPvm.replace(/\s+/g, '')); // ruokaPvm.replace(/\s+/g, '');
+						food.normal.push(ruoka); // ruoka
+						food.vege.push(kasvisruoka.replace(/ {2}Kasvisruoka /g, '')); // kasvisruoka.replace(/ {2}Kasvisruoka/g, '')
+					}
+				});
+
+				console.log(
+					info + `Fetched data in`,
+					Date.now() - start,
+					`ms at ${moment().format('MMMM Do YYYY, h:mm:ss a')}`
+				);
+
+				resolve(food);
+			})
+			.catch((err) => {
+				console.warn('Failed to fetch', err);
+				resolve(undefined);
+			});
+	});
+
+const getTodayDateNum = () => {
+	if (moment().locale('fi').isoWeekday() - 1 >= 5) {
+		return 4;
+	} else {
+		return moment().locale('fi').isoWeekday() - 1;
+	}
+};
+
+const createVideo = async (inputProps?: object | undefined) => {
 	const tmpDir = path.resolve(process.env.VIDEO_DIR || 'videos');
 	await fs.promises.mkdir(tmpDir, {recursive: true});
 	const key = moment(new Date()).format('YYYYMMDD') + '.mp4';
 	const finalOutput = path.join(tmpDir, 'ruoka' + key);
 
+	const start = Date.now();
+
 	if (fs.existsSync(finalOutput)) {
 		return finalOutput;
 	} else {
+		const bundled = await bundle(path.join(__dirname, './src/index.tsx'));
+		const comps = await getCompositions(bundled, {inputProps});
+		const video = comps.find((c) => c.id === compositionId);
+		if (!video) {
+			throw new Error(`No video called ${compositionId}`);
+		}
+
 		const {assetsInfo} = await renderFrames({
 			config: video,
 			webpackBundle: bundled,
@@ -72,12 +132,25 @@ const createVideo = async (inputProps?: object | undefined) => {
 			imageFormat: 'jpeg',
 			assetsInfo,
 		});
+
+		fs.readdirSync('./videos').map((file) => {
+			if (file.endsWith('.jpeg')) {
+				fs.unlinkSync('./videos/' + file);
+			}
+
+			if (file.startsWith('ruoka') && !file.endsWith(key)) {
+				fs.unlinkSync('./videos/' + file);
+			}
+		});
 	}
+
+	console.log(success + 'Created video in', Date.now() - start, 'ms');
 
 	return finalOutput;
 };
 
 const cacheVideo = async (req: Request, res: Response) => {
+	const start = Date.now();
 	const sendFile = (file: string) => {
 		fs.createReadStream(file)
 			.pipe(res)
@@ -87,25 +160,12 @@ const cacheVideo = async (req: Request, res: Response) => {
 	};
 
 	try {
-		if (cache.get(JSON.stringify(req.query))) {
-			sendFile(cache.get(JSON.stringify(req.query)) as string);
-			return;
-		}
-
 		res.set('content-type', 'video/mp4');
 
 		const finalOutput = await createVideo();
 		sendFile(finalOutput);
 
-		console.log(success + 'Video rendered and sent!');
-
-		fs.readdirSync('./videos').map((f) => {
-			if (f.endsWith('.jpeg')) {
-				fs.unlinkSync('./videos/' + f);
-			} else {
-				return;
-			}
-		});
+		console.log(success + 'Sent video in', Date.now() - start, 'ms');
 	} catch (err) {
 		console.log(error + 'An error has occured...');
 		console.error(err);
@@ -115,20 +175,100 @@ const cacheVideo = async (req: Request, res: Response) => {
 	}
 };
 
+let food: IFood | undefined = undefined;
+
+app.get('/cors', async (req, res) => {
+	res.set('Access-Control-Allow-Origin', '*');
+
+	if (!food) {
+		food = await fetchData();
+	}
+
+	if (!food) {
+		return res.status(500).send({});
+	}
+
+	fs.writeFileSync(
+		`./videos/${moment(new Date()).format('YYYYMMDD')}.json`,
+		JSON.stringify(food)
+	);
+
+	fs.readdirSync('./videos').map(async (file) => {
+		if (
+			file.endsWith('.json') &&
+			file.replace('.json', '') !== moment(new Date()).format('YYYYMMDD')
+		) {
+			food = await fetchData();
+			fs.writeFileSync(
+				`./videos/${moment(new Date()).format('YYYYMMDD')}.json`,
+				JSON.stringify(food)
+			);
+
+			if (fs.existsSync('./videos/' + file)) {
+				fs.unlinkSync('./videos/' + file);
+			}
+		}
+	});
+
+	res.send({
+		status_code: res.statusCode,
+		status_message: res.statusMessage || null,
+		time_now: Date.now(),
+		url,
+		data: {
+			menu: {
+				food,
+			},
+			menu_today: {
+				food: {
+					num_date: getTodayDateNum(),
+					today_date_full: moment()
+						.locale('fi')
+						.format('MMMM Do YYYY, h:mm:ss a'),
+					today_date: moment().locale('fi').format('l'),
+					current_week: moment().locale('fi').format('w'),
+					date: food.date[getTodayDateNum()] || null,
+					normal: food.normal[getTodayDateNum()] || null,
+					vege: food.vege[getTodayDateNum()] || null,
+				},
+			},
+		},
+	});
+});
+
 app.get('/', async (req, res) => {
 	cacheVideo(req, res);
 });
 
-app.listen(port);
+app.listen(port, async () => {
+	food = await fetchData();
+	fs.writeFileSync(
+		`./videos/${moment(new Date()).format('YYYYMMDD')}.json`,
+		JSON.stringify(food)
+	);
 
-console.log(success + `Server is running on http://localhost:${port}.`);
+	console.info(info + `Creating & sending the video...`);
+	await createVideo();
+	console.log(success + 'Sent video!');
 
-axios
-	.get(`http://localhost:${port}`)
-	.then(() => {
-		console.log(success + 'Successfully fetched the API!');
-	})
-	.catch((err) => {
-		console.log(error + 'An error has occured...');
-		console.error(err);
-	});
+	const continent = process.env.CRON_CONT || 'Europe';
+	const city = process.env.CRON_CITY || 'Helsinki';
+
+	const job = new CronJob(
+		'0 7 * * *',
+		async () => {
+			await fetchData();
+
+			console.info(info + `Creating & sending the video...`);
+			await createVideo();
+			console.log(success + 'Sent video!');
+		},
+		null,
+		true,
+		`${continent + '/' + city}`
+	);
+
+	job.start();
+});
+
+console.info(success + `Server is running on http://localhost:${port}.`);
---
+++ videos/README.md
@@ -0,0 +1,3 @@
+# ./videos/ folder
+
+This is where your videos get exported when running `server.tsx` file.
